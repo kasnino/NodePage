36ace6ba5e602aef72988bd10f14d8a20da6f529
kasnino <kasn.emprendedor@gmail.com>
2016-12-03 02:14:48 -0400

    Servidor nodeExpress

--- opp.js
+++
@@ -1,9 +0,0 @@
-// modulos
-  const express =  require('express');
-  const app = express();
-
-  require('./config/express')(app, express);
-  require('./route/routes')(app);
-
-  //app.use();
-  app.listen(3000);
